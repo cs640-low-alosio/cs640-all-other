aa722def2e5c566467887e0350a4e134b3cfcae1
glow <usernamegl@live.com>
2021-02-20 10:50:37 -0600

    a2p2: first attempt

--- a2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
+++ a2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -1,9 +1,11 @@
 package edu.wisc.cs.sdn.vnet.sw;
 
 import net.floodlightcontroller.packet.Ethernet;
-
-import java.util.HashTable;
-
+import net.floodlightcontroller.packet.MACAddress;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 import edu.wisc.cs.sdn.vnet.Device;
 import edu.wisc.cs.sdn.vnet.DumpFile;
 import edu.wisc.cs.sdn.vnet.Iface;
@@ -11,40 +13,55 @@ import edu.wisc.cs.sdn.vnet.Iface;
 /**
  * @author Aaron Gember-Jacobson
  */
-public class Switch extends Device
-{	
-  private HashTable<Iface, SwitchEntry> switchTable;
-  
-	/**
-	 * Creates a router for a specific host.
-	 * @param host hostname for the router
-	 */
-	public Switch(String host, DumpFile logfile)
-	{
-		super(host,logfile);
-    this.switchTable = new HashTable<Iface, SwitchEntry>();
-	}
+public class Switch extends Device {
+  private HashMap<MACAddress, SwitchEntry> switchTable;
 
-	/**
-	 * Handle an Ethernet packet received on a specific interface.
-	 * @param etherPacket the Ethernet packet that was received
-	 * @param inIface the interface on which the packet was received
-	 */
-	public void handlePacket(Ethernet etherPacket, Iface inIface)
-	{
-		System.out.println("*** -> Received packet: " +
-				etherPacket.toString().replace("\n", "\n\t"));
-		
+  /**
+   * Creates a router for a specific host.
+   * 
+   * @param host hostname for the router
+   */
+  public Switch(String host, DumpFile logfile) {
+    super(host, logfile);
+    this.switchTable = new HashMap<MACAddress, SwitchEntry>();
+  }
+
+  /**
+   * Handle an Ethernet packet received on a specific interface.
+   * 
+   * @param etherPacket the Ethernet packet that was received
+   * @param inIface     the interface on which the packet was received
+   */
+  public void handlePacket(Ethernet etherPacket, Iface inIface) {
+    System.out.println("*** -> Received packet: " + etherPacket.toString().replace("\n", "\n\t"));
+
     MACAddress sourceMac = etherPacket.getSourceMAC();
     MACAddress destMac = etherPacket.getDestinationMAC();
-    SwitchEntry inSwitchEntry = new SwitchEntry(sourceMac, inIface);  
-        
-    switchTable.insert(inIface, inSwitchEntry);
+    // SwitchEntry inSwitchEntry = new SwitchEntry(sourceMac, inIface);
+    switchTable.put(destMac, new SwitchEntry(sourceMac, inIface)); // Reset entry whenever switch
+                                                                   // receives new packet
+    // should we handle three cases separately?
+    // interface is not in switch table
+    // interface is in table and not expired,
+    // interface is in table and expired (basically same as first case)
     
-        
-		/********************************************************************/
-		/* TODO: Handle packets                                             */
-		
-		/********************************************************************/
-	}
+    SwitchEntry outEntry = null;
+    if ((outEntry = switchTable.get(destMac)) != null ) {
+      if (outEntry.getIface() == inIface) { // drop frame if inIface same as outIface
+        return;
+      } else {
+        sendPacket(etherPacket, outEntry.getIface());
+      }
+    } else { // flood all, except incoming interface
+      Set<String> ifaceNameSet = interfaces.keySet();
+      for (String ifaceName : ifaceNameSet) {
+        sendPacket(etherPacket, interfaces.get(ifaceName));
+      }
+    }
+
+    /********************************************************************/
+    /* TODO: Handle packets */
+
+    /********************************************************************/
+  }
 }
--- a2/src/edu/wisc/cs/sdn/vnet/sw/SwitchEntry.java
+++ a2/src/edu/wisc/cs/sdn/vnet/sw/SwitchEntry.java
@@ -1,36 +1,34 @@
 package edu.wisc.cs.sdn.vnet.sw;
 
-import net.floodlightcontroller.packet.Ethernet;
-
-import edu.wisc.cs.sdn.vnet.Device;
-import edu.wisc.cs.sdn.vnet.DumpFile;
+import net.floodlightcontroller.packet.MACAddress;
 import edu.wisc.cs.sdn.vnet.Iface;
 
 public class SwitchEntry {
   private Iface iface;
   private MACAddress macAddr;
-  private int ttl;
-  private static int DEFAULT_TTL=15;
-  
+  private long startTime;
+  // private int ttl;
+  // private static int DEFAULT_TTL=15;
+
   public SwitchEntry(MACAddress macAddr, Iface iface) {
     this.macAddr = macAddr;
     this.iface = iface;
-    this.ttl = DEFAULT_TTL;
+    this.startTime = System.nanoTime() / 1000000000;
   }
-  
-  public getMacAddr() {
+
+  public MACAddress getMacAddr() {
     return this.macAddr;
   }
-  
-  public getIface() {
+
+  public Iface getIface() {
     return this.iface;
   }
-  
-  public getTtl() {
-    return this.ttl;
+
+  public long getStartTime() {
+    return this.startTime;
   }
-  
-  public setTtl(int ttl) {
-    this.ttl = ttl;
-  }
+
+  // public void setTtl(int ttl) {
+  // this.ttl = ttl;
+  // }
 }
