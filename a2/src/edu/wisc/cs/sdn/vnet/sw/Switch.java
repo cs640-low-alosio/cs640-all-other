ade9f9e1d973f41617443551e688bdb9b02495a3
glow <usernamegl@live.com>
2021-02-21 06:22:04 -0600

    a2p2: thread attempt

--- a2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
+++ a2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -4,6 +4,8 @@ import net.floodlightcontroller.packet.Ethernet;
 import net.floodlightcontroller.packet.MACAddress;
 import java.util.HashMap;
 import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
 import edu.wisc.cs.sdn.vnet.Device;
 import edu.wisc.cs.sdn.vnet.DumpFile;
 import edu.wisc.cs.sdn.vnet.Iface;
@@ -13,6 +15,7 @@ import edu.wisc.cs.sdn.vnet.Iface;
  */
 public class Switch extends Device {
   private HashMap<MACAddress, SwitchEntry> switchTable;
+  static int counter = 0;
 
   /**
    * Creates a router for a specific host.
@@ -22,6 +25,18 @@ public class Switch extends Device {
   public Switch(String host, DumpFile logfile) {
     super(host, logfile);
     this.switchTable = new HashMap<MACAddress, SwitchEntry>();
+    TimerTask timerTask = new TimerTask() {
+
+      @Override
+      public void run() {
+        System.out.println("TimerTask executing counter is: " + counter);
+        counter++;// increments the counter
+      }
+    };
+
+    Timer timer = new Timer("MyTimer");// create a new Timer
+
+    timer.scheduleAtFixedRate(timerTask, 30, 3000);
   }
 
   /**
@@ -37,17 +52,18 @@ public class Switch extends Device {
     MACAddress destMac = etherPacket.getDestinationMAC();
     // SwitchEntry inSwitchEntry = new SwitchEntry(sourceMac, inIface);
     // Reset switch table entry for source MACAddr and interface
-    switchTable.put(sourceMac, new SwitchEntry(sourceMac, inIface)); 
-    
+    switchTable.put(sourceMac, new SwitchEntry(sourceMac, inIface));
+
     // should we handle three cases separately?
     // interface is not in switch table
     // interface is in table and not expired,
     // interface is in table and expired (basically same as first case)
-    
+
     SwitchEntry outEntry = null;
-    if ((outEntry = switchTable.get(destMac)) != null ) {
+    if ((outEntry = switchTable.get(destMac)) != null) {
       System.out.println("DEBUG: matching switch table entry found!"); // debug
-      System.out.println("DEBUG: dest mac: " + destMac + ", switch table entry: "+ outEntry.getIface()); // debug
+      System.out
+          .println("DEBUG: dest mac: " + destMac + ", switch table entry: " + outEntry.getIface()); // debug
       if (outEntry.getIface() == inIface) { // drop frame if inIface same as outIface
         // use VNSComm.java etherAddrsMatchInterface() instead?
         return;
@@ -64,7 +80,7 @@ public class Switch extends Device {
         System.out.println("DEBUG: flood trying: " + faceName); // debug
         if (sendPacket(etherPacket, interfaces.get(faceName))) {
           System.out.println("DEBUG: sending to ifacename: " + faceName); // debug
-//          break; // break when sendPacket is true?
+          // break; // break when sendPacket is true?
         }
       }
     }
