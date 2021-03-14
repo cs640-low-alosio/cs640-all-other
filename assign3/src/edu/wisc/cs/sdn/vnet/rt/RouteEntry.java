1161e22c971147cb3df162ed7d77bafdc5ae0244
Garrett Low <usernamegl@live.com>
2021-03-14 10:46:28 -0500

    a3: setting up threads

--- assign3/src/edu/wisc/cs/sdn/vnet/rt/RouteEntry.java
+++ assign3/src/edu/wisc/cs/sdn/vnet/rt/RouteEntry.java
@@ -99,7 +99,7 @@ public class RouteEntry {
   }
 
   public String toString() {
-    return String.format("%s \t%s \t%s \t%s \t%i", IPv4.fromIPv4Address(this.destinationAddress),
+    return String.format("%s \t%s \t%s \t%s \t%s", IPv4.fromIPv4Address(this.destinationAddress),
         IPv4.fromIPv4Address(this.gatewayAddress), IPv4.fromIPv4Address(this.maskAddress),
         this.iface.getName(), this.ttl);
   }
