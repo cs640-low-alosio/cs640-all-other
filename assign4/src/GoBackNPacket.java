e3277f598b6049cab6c9f428c8e86152fd7dbd01
Garrett Low <usernamegl@live.com>
2021-03-28 11:08:08 -0500

    a4: calculate checksum, serialize segment

--- assign4/src/GoBackNPacket.java
+++ assign4/src/GoBackNPacket.java
@@ -1,31 +1,80 @@
+import java.nio.ByteBuffer;
+
 /**
  * 
  * @author Garrett
  *
  */
 public class GoBackNPacket {
+  static final int HEADER_LENGTH_BYTES = 24;
+  
   protected int byteSequenceNum;
   protected int ackNum;
   protected long timestamp;
-  protected int totalLength;
+//  protected int totalLength;
   protected int dataLength;
   protected boolean isSyn;
   protected boolean isAck;
   protected boolean isFin;
   protected short checksum;
-  protected byte[] data;
+  protected byte[] payloadData;
   
   public byte[] serialize() {
+    int totalLength =  payloadData.length + HEADER_LENGTH_BYTES;
+    byte[] allSegmentData = new byte[totalLength];
     
-    byte[] data = new byte[this.totalLength];
+    ByteBuffer bb = ByteBuffer.wrap(allSegmentData);
+    bb.putInt(byteSequenceNum);
+    bb.putInt(ackNum);
+    bb.putLong(timestamp);
     
-    return data;
+    // Length and flags word
+    int lengthAndFlags = 0b0;
+    lengthAndFlags = dataLength << 3;
+    if (isSyn) {
+      lengthAndFlags += (0b1 << 2);       
+    }
+    if (isFin) {
+      lengthAndFlags += (0b1 << 1);
+    }
+    if (isAck) {
+      lengthAndFlags += (0b1 << 0);
+    }
+    bb.putInt(lengthAndFlags);
+    
+    bb.putInt(0x0000);    
+    
+    if (dataLength != 0) {
+      bb.put(payloadData);
+    }
+    
+    // Calculate checksum
+    bb.rewind();
+    int tempSum = 0;
+    for (int i = 0; i < allSegmentData.length / 2; i++) {
+      tempSum += bb.getShort();
+    }
+    if (allSegmentData.length % 2 == 1) { // there is an extra byte at the end
+      tempSum += (bb.get() & 0xff) << 8;
+    }
+    // Handle carry-over
+    while (tempSum > 0xffff) {
+      int carryoverBits = tempSum >> 16;
+      int lastSixteenBits = tempSum - ((tempSum >> 16) << 16);
+      tempSum = lastSixteenBits + carryoverBits;
+    }
+    this.checksum = (short) (~tempSum & 0xffff);
+    
+    bb.putShort(22, this.checksum);
+    
+    return allSegmentData;
   }
   
   public GoBackNPacket deserialize(byte[] data) {
+    ByteBuffer byteBuffer = ByteBuffer.wrap(data);
     return null;
   }
-  
+
   public int getByteSequenceNum() {
     return byteSequenceNum;
   }
