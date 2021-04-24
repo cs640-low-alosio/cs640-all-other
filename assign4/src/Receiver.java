fcf79c2868f77a441b1d04975aa0da6af4fd46f6
Garrett Low <usernamegl@live.com>
2021-04-24 18:31:16 -0500

    a4: rcvr discard data that's already written

--- assign4/src/Receiver.java
+++ assign4/src/Receiver.java
@@ -143,7 +143,7 @@ public class Receiver extends TCPEndHost {
         // Check if received packet is within SWS
         if (currBsn >= firstByteBeyondSws) {
           // Discard out-of-order packets (outside sliding window size)
-          System.out.println("Rcv - discard out-of-order packet!!!");
+          System.err.println("Rcv - discard out-of-order packet!!!");
           GBNSegment ackSegment =
               GBNSegment.createAckSegment(bsn, nextByteExpected, mostRecentTimestamp);
           sendPacket(ackSegment, senderIp, senderPort);
@@ -153,6 +153,10 @@ public class Receiver extends TCPEndHost {
           // When this condition was part of the discard out-of-order packet
           // and send ACK case above, we were sending a ton of duplicate ACKs which was causing
           // a ton of extra traffic
+          System.err.println("Rcv - discard out-of-order packet!!!");
+          GBNSegment ackSegment =
+              GBNSegment.createAckSegment(bsn, nextByteExpected, mostRecentTimestamp);
+          sendPacket(ackSegment, senderIp, senderPort);
           continue;
         } else {
           // Add packets to buffer if within sliding window size
